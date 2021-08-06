9dcd3c352510e6980c2d555253e38a30308e6a82
asceznyk <asceznyk@gmail.com>
2021-08-06 11:10:42 +0530

    mistakes happen bitch!

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Discriminator(nn.Module):
         self.img_dim = img_dim
         self.disc = nn.Sequential(
             nn.Linear(img_dim, 128),
-            nn.LeakyRELU(0.01),
+            nn.LeakyReLU(0.01),
             nn.Linear(128, 1),
             nn.Sigmoid()
         )
@@ -61,7 +61,7 @@ class Generator(nn.Module):
         self.z_dim = z_dim
         self.gen = nn.Sequential(
             nn.Linear(z_dim, 256),
-            nn.LeakyRELU(0.01),
+            nn.LeakyReLU(0.01),
             nn.Linear(256, img_dim),
             nn.Tanh()
         )
