3eea4dd2700adca8531e61a496c48b2d47441433
asceznyk <asceznyk@gmail.com>
2021-08-06 10:55:14 +0530

    mistakes happen bitch!

--- main.py
+++ main.py
@@ -1,15 +1,45 @@
+import os
 import math
 import argparse
+import PIL
+
 import numpy as np
+
+from PIL import Image
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 import torchvision
+import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
+
+def get_random_noise(dim, batch_size=1): return torch.randn((batch_size, dim))
+
+def get_basic_transform():
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((size, size))
+        transforms.Normalize(((0.5,), (0.5,), )),
+    ])
+
+class ImageDir(Dataset):
+    def __init__(self, dirctry, transform, z_dim=64):
+        self.dirctry = dirctry
+        self.files = [dirctry+'/'+f for f in os.listdir(dirctry)]
+        self.transform = transform
+
+        self.z_dim = z_dim
+
+    def __getitem__(self, i):
+        noise = get_random_noise(self.z_dim)[0]
+        img = Image.open(self.files[0])
+        img = self.transform(img)
+
+        return img, noise
 
 class Discriminator(nn.Module):
     def __init__(self, img_dim):
