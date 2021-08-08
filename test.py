049f07d71ad3686589eec69c4bfdbc033724962f
asceznyk <asceznyk@gmail.com>
2021-08-08 12:05:33 +0530

    major update!

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ disc = DCDisc(img_dim)
 out = disc(torch.randn(1, *img_dim))
 print(out, out.size())
 
-m1 = nn.ConvTranspose2d(100, 1024, 5, 2, 1)
-m2 = nn.ConvTranspose2d(1024, 512, 5, 2, 1)
+m1 = nn.ConvTranspose2d(100, 1024, 8, 2, 2)
+m2 = nn.ConvTranspose2d(1024, 512, 8, 2, 2)
 
 inp = torch.randn(1, 100, 1, 1)
 
