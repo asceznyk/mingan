86ec188c756f8322db11eb0981174cc91d510af3
asceznyk <asceznyk@gmail.com>
2021-08-08 18:19:52 +0530

    major update!

--- main.py
+++ main.py
@@ -64,7 +64,8 @@ def plt_imgs(imgs, batch_size, save_path):
 def init_dataset(mode, tsfm):
     root = 'dataset/'
     if mode == 'dcgan':
-        dataset = datasets.CIFAR10(root=root, train=False, download=True, transform=tsfm)
+        #dataset = datasets.CIFAR10(root=root, train=False, download=True, transform=tsfm)
+        dataset = datasets.FashionMNIST(root=root, train=True, transform=tsfm, download=True)
     else:
         dataset = datasets.FashionMNIST(root=root, transform=tsfm, download=True)
     return dataset
